b7a3b067fd6aa5707a92563496ff2871f716474d
Zahra Syyida <zsyyida@lab2r1s3.42abudhabi.ae>
2023-11-09 15:28:10 +0400

    startex01

--- ex01/Form.cpp
+++ ex01/Form.cpp
@@ -6,7 +6,7 @@
 /*   By: zsyyida <zsyyida@student42abudhabi.ae>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 13:42:59 by zsyyida           #+#    #+#             */
-/*   Updated: 2023/11/09 15:16:20 by zsyyida          ###   ########.fr       */
+/*   Updated: 2023/11/09 15:27:48 by zsyyida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,20 +29,32 @@ Form::Form() : _name("name"), _signStatus(0), _gradeSign(1), _gradeExecute(2)
 
 Form::Form(std::string name, bool a, int b, int c) : _name(name), _signStatus(a), _gradeSign(b), _gradeExecute(c)
 {
-	const_cast<std::string&>(this->_name) = name;
-	this->_signStatus = a;
-	const_cast<int&>(this->_gradeSign) = b;
-	const_cast<int&>(this->_gradeExecute) = c;
+	setFormName(name);
+	setSignStatus(a);
+	setGradeSign(b);
+	setGradeExecute(c);
+	// const_cast<std::string&>(this->_name) = name;
+	// this->_signStatus = a;
+	// const_cast<int&>(this->_gradeSign) = b;
+	// const_cast<int&>(this->_gradeExecute) = c;
+	std::cout << Form::getFormName() << " form parametric constructor called." << std::endl;
 }
 
 Form::Form(const Form &object)
 {
-
+	*this = object;
+	std::cout << "Form copy constructor called." << std::endl;
 }
 
 Form &Form::operator=(const Form &object)
 {
-
+	if (this != &object)
+	{
+		const_cast<std::string&>(this->_name) = object.getFormName();
+		_grade = object._grade;
+	}
+	std::cout << "Bureaucrat copy assignment operator called." << std::endl;
+	return (*this);
 }
 
 Form::~Form()
@@ -87,12 +99,12 @@ void Form::setSignStatus(bool signStatus)
 
 void Form::setGradeSign(const int gradeSign)
 {
-	this->_gradeSign = gradeSign;
+	const_cast<int&>(this->_gradeSign) = gradeSign;
 }
 
 void Form::setGradeExecute(const int gradeExecute)
 {
-	this->_gradeExecute = gradeExecute;
+	const_cast<int&>(this->_gradeExecute) = gradeExecute;
 }
 
 void Form::beSigned(Bureaucrat)
